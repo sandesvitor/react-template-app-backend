02907c2f3b840279c68cb15671d28ea9f402eed5
Vitor Sandes <sandesvitor@gmail.com>
2020-09-12 01:50:01 -0300

    First Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const path = require('path')
+// const multer = require('multer') ---> para Upload de arquivos!
+
+const state = {
+    input: ''
+}
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.post('/', (req, res, next) => {
+    let message = req.body.input
+    state.input = message
+    console.debug('New input on frontend: [%s]', message)
+})
+
+app.listen(8080, () => {
+    console.log('Server up on port 8080')
+})
