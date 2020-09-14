83d7a03f1318ef932eaa72d3cc6e91666f60fc21
Vitor Sandes <sandesvitor@gmail.com>
2020-09-14 00:41:40 -0300

    Implemented temp database folder with GET and POST request to json files

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+const fs = require('fs')
+const port = 3333
+
 const cors = require('cors')
 // const multer = require('multer') ---> para Upload de arquivos!
 
-const state = {
-    input: null,
-    title: null
-}
 
 // alowwing access from cross-origin:
 app.use((req, res, next) => {
@@ -27,36 +26,19 @@ app.get('/', (req, res) => {
 })
 
 // MUDAR TÍTULO DA NAVBAR
-app.post('/title', (req, res, next) => {
-    let info = req.body
-    state.title = debug
-    console.debug('New title received: [%s]', debug)
-    next()
-})
-
 app.get('/title', (req, res, next) => {
-    res.send(state.title)
-    next()
+    res.sendFile(path.join(__dirname + '/DB_temp/state.json'))
 })
 
+app.post('/title', (req, res, next) => {
+    let title = req.body
+    fs.writeFileSync('./DB_temp/state.json', JSON.stringify(title, null, 2))
 
-
-// TESTE DE CONEXÃO!
-app.post('/info', (req, res, next) => {
-    let info = req.body
-    state.input = info
-    console.debug('New input on frontend: [%s]', info)
-    next()
-})
-
-app.get('/info', (req, res, next) => {
-    if (state.input !== null) {
-        res.send(state.input)
-    }
-    next()
+    console.debug('Wrinting title on JSON file:')
+    console.debug(title)
 })
 
 // PORT
-app.listen(8080, () => {
-    console.log('Server up on port 8080')
+app.listen(port, () => {
+    console.log(`Server up on port ${port}`)
 })
